d640f0a115e4bc20fb9a460424feec15d61e0286
root <root@ip-172-31-47-20.ap-south-1.compute.internal>
2025-03-05 01:26:33 +0000

    jjss

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('WORKING PIPELINE ON DATED 5TH OF MARCH 2025'));
+app.get('/', (req, res) => res.send('WORKING PIPELINE ON MARCH 2025'));
 app.listen(3000, () => console.log('Server running on port 3000'));
