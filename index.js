7fb78cab2d991cd97df7d14f1471f6e3c4317cd6
root <root@ip-172-31-47-20.ap-south-1.compute.internal>
2025-03-05 00:32:37 +0000

    wqdd

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello'));
+app.get('/', (req, res) => res.send('jwdkwendkHelloagain'));
 app.listen(3000, () => console.log('Server running on port 3000'));
