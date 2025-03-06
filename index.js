29285069cdeda496afbd6c2528c6f727ef94d191
root <root@ip-172-31-47-20.ap-south-1.compute.internal>
2025-03-06 05:21:04 +0000

    HDDJFJDFJ

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('pipeline running on 6th of march'));
+app.get('/', (req, res) => res.send('pipeline running on 6th of march running ccooddee'));
 app.listen(3000, () => console.log('Server running on port 3000'));
